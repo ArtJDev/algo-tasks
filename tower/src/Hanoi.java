1feace743b733cb2b05069aa03e8ef0b2ad32f3e
ArtJDev <artemkurilenko1983@yandex.ru>
2023-02-16 19:25:29 +0300

    Ханойские башни

--- tower/src/Hanoi.java
+++ tower/src/Hanoi.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Hanoi {
+import java.util.Stack;
+
+public class Hanoi {
+    private final int numDiscs;
+    public final Stack<Integer> towerA = new Stack<>();
+    public final Stack<Integer> towerB = new Stack<>();
+    public final Stack<Integer> towerC = new Stack<>();
+    public Hanoi(int discs) {
+        numDiscs = discs;
+        for (int i = 1; i <= discs; i++) {
+            towerA.push(i);
+        }
+    }
+
+    private void move(Stack<Integer> begin, Stack<Integer> end, Stack<Integer> temp, int n) {
+        if (n == 1) {
+            end.push(begin.pop());
+        } else {
+            move(begin, temp, end, n - 1);  //Переместить верхние n - 1 дисков с башни A(begin) на башню B(temp), используя C(end) в качестве промежуточной башни
+            move(begin, end, temp, 1);      //Переместить нижний диск с A(begin) на C(end)
+            move(temp, end, begin, n - 1);  //Переместить n - 1 дисков с башни B(temp) на башню C(end), башня A(begin) — промежуточная
+        }
+    }
+
+    public void solve() {
+        move(towerA, towerC, towerB, numDiscs);
+    }
+
+    public static void main(String[] args) {
+        Hanoi hanoi = new Hanoi(5);
+        hanoi.solve();
+        System.out.println(hanoi.towerA);
+        System.out.println(hanoi.towerB);
+        System.out.println(hanoi.towerC);
+    }
 }
